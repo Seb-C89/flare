e19a623048902f48e55b436bfc4037f27d256235
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-11-10 18:57:28 +0100

    viewer animation

--- target/script.js
+++ target/script.js
@@ -1,8 +1,15 @@
 function show_viewer(e){
 	document.getElementById('Viewer').style.display = "block";
 	document.getElementById('Viewport').src = e.getElementsByTagName('img')[0].src;
+	document.getElementById('Viewer').style.animation = "fade_in 0.25s cubic-bezier(0,0,0,1) 1 forwards";
 }
 
-function hide_viewer(){
-	document.getElementById('Viewer').style.display = "none";
+function hide_viewer(a){
+	a.addEventListener("AnimationEnd", remove_viewer(a));
+	a.style.animation = "fade_out 0.25s cubic-bezier(0,0,0,1) 1 forwards";
+}
+
+function remove_viewer(a) {
+	a.style.display = "none";
+	a.removeEventListener("AnimationEnd", remove_viewer(a));
 }
