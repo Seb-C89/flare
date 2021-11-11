ae77b6819960d52a65153d13899aef98ca1060b8
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-11-11 18:08:55 +0100

    works good

--- target/script.js
+++ target/script.js
@@ -1,31 +1,34 @@
+console.log(document.getElementById("Viewer"));
+
 function show_viewer(e){
+	//document.getElementById('Viewer').addEventListener("animationend", l);
+	console.log("fade_in")
 	document.getElementById('Viewer').style.display = "block";
 	document.getElementById('Viewport').src = e.getElementsByTagName('img')[0].src;
+	
 	document.getElementById('Viewer').classList.toggle("fade_in");
 	document.getElementById('Viewer').classList.toggle("fade_out", false);
-	console.log("fade_in");
-	//document.getElementById('Viewer').classList.toggle("fade_in");
-	//document.getElementById('Viewer').style.animation = "fade_in 0.25s cubic-bezier(0,0,0,1) 1 forwards";
-	//document.getElementById('Viewer').style.animation = "fade_in 0.25s ease-out 1 forwards";
-	//document.getElementById('Viewer').style.animation = "fade_out 10s ease-out 1 forwards";
+
+	document.getElementById('Viewer').addEventListener("click", hide_viewer);
 }
 
-function hide_viewer(a){
-	console.log("fade_out");
-	a.addEventListener("AnimationEnd", (a) => remove_viewer(a));
-	//document.getElementById('Viewer').style.animationPlayState = "paused";
-	//document.getElementById('Viewer').style.animation = "fade_out 010s ease-out 0s 1 normal forwards";
-	//document.getElementById('Viewer').style.animationPlayState = "running";
-	//a.style.animation = "none";
-	//a.classList.toggle("fade_in", false);
-	//document.getElementById('Viewer').style.animation = "fade_in 10s ease-out 0s 1 reverse backwards";
-	//a.classList.toggle("fade_out");
-	document.getElementById('Viewer').classList.toggle("fade_in", false);
-	document.getElementById('Viewer').classList.toggle("fade_out");
+function hide_viewer(){
+	console.log("fade_out", this)
+	this.removeEventListener("click", hide_viewer);
+	this.addEventListener("animationend", remove_viewer);
+
+	this.classList.toggle("fade_in", false);
+	this.classList.toggle("fade_out");
 }
 
-function remove_viewer(a) {
-	console.log("animation_end");
-	a.style.display = "none";
-	a.removeEventListener("AnimationEnd", remove_viewer(a));
+function remove_viewer() {
+	console.log("animation end", this)
+	this.style.display = "none";
+	this.removeEventListener("animationend", remove_viewer);
 }
+
+/*function l() {
+	console.log('ttttttt')
+	document.getElementById('Viewer').addEventListener("click", hide_viewer);
+	document.getElementById('Viewer').removeEventListener("animationend", l);
+}*/
