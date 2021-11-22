8c658adf82e7181fcdaf2415511351151524ef4d
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-11-22 14:36:13 +0100

    good

--- component/Menu_game.js
+++ component/Menu_game.js
@@ -1,8 +1,8 @@
 export default function(props){
-	let a = [{"game":"battlefield 3","count":2},{"game":"serious sam: the second encounter","count":1}]
+	let { games } = props
 	
-	let rows = a.map((v) => {
-		return <li>{ ""+v.game+" ("+v.count+")" }</li>
+	let rows = games.map((value, index) => {
+		return <li key={ "game"+index }>{ ""+value.game+" ("+value.count+")" }</li>
 	})
 
 	return <ul>
--- pages/test.js
+++ pages/test.js
@@ -1,5 +1,15 @@
 import Game_menu from "../component/Menu_game.js"
+import { get_games, get_recent } from "../utils/db.js"
 
-export default function(){
-	return <Game_menu />
+export default function(props){
+	return <Game_menu games={props.games} />
 }
+
+export async function getServerSideProps(context) {
+	return {
+	  props: { 
+		  games : await get_games().then((data) => data).catch(() => []),
+		  posts : await get_recent().then((data) => data).catch(() => [])
+		}, // will be passed to the page component as props
+	}
+  }
