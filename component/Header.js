c627cfbc007820d13e30e07608482bcf0ef1139e
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-12-17 14:37:35 +0100

    add links

--- component/Header.js
+++ component/Header.js
@@ -5,11 +5,11 @@ export default function (props){
 		<h1 style={{order: 2}}>Lens flare</h1>
 		<ul>
 			<li style={{order: 1}}>
-				<a href="#">Galerie</a></li>
+				<Link href="/gallery"><a>Galerie</a></Link></li>
 			<li style={{order: 3}}>
 				<Link href="/about"><a>A propos</a></Link></li>
 			<li style={{order: 3}}>
-				<a href="#">Contact</a></li>
+				<Link href="/contact"><a>Contact</a></Link></li>
 		</ul>
 	</header>
 }
