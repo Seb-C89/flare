d9a874d4bc975746dc603c421b8924b0b863f338
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2021-12-26 18:13:19 +0100

    class <> className

--- component/Card.js
+++ component/Card.js
@@ -3,10 +3,10 @@ import Image from 'next/image' // TODO
 export default function (props){
 	const { image, user_name, game, date } = props.post
 
-	return <article class="card">
+	return <article className="card">
 		<img src={ "/img/"+image+".jpg" } alt="screenshoot" />
 		<footer>
-			<p>In <span class="game">{ game }</span> posted by <span class="author">{ user_name }</span> at <span class="date">{ date }</span></p>
+			<p>In <span className="game">{ game }</span> posted by <span class="author">{ user_name }</span> at <span class="date">{ date }</span></p>
 		</footer>
 	</article>
 }
