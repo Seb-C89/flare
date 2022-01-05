12e3ea3b799093185167c0ef9afea32fd6d1fff5
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-01-05 14:03:03 +0100

    wip

--- pages/api/form/post.js
+++ pages/api/form/post.js
@@ -1,5 +1,7 @@
 const formidable = require('formidable');
 const os = require('os'); // remove
+import { fileTypeFromFile } from 'file-type'
+const fs = require('fs')
 
 export const config = {
 	api: {
@@ -7,22 +9,30 @@ export const config = {
 	},
 };
 
-function aaa(){
-	console.log("do smthg with file")
-}
-
 export default async function endpoint(req, res) {
 	const form = formidable({ 
-		multiples: true,
+		multiples: false,
 		uploadDir: "uploads/",
 		//fileWriteStreamHandler: aaa // function who receive files data and save it
 	});
 
-	form.parse(req, (err,fields,files) => {
+	form.parse(req, async (err,fields,files) => {
 		console.log(err)
 		console.log(fields)
-		console.log(files.length)
+		console.log(files.file.size)
 		console.log(os.tmpdir())
+		let res = await fileTypeFromFile(files.file.filepath)
+			.then((res) => {
+				console.log(res.mime)
+				console.log(res.ext)
+				//fs.rename(files.file.filepath, files.file.filepath+'_'+res.ext)
+				return res.ext
+			})
+			.catch(() => {
+				console.log('Failled to detecte file type')
+			})
+		console.log(res.mime)
+		console.log(res.ext)
 	})
 
 	res.json('file recieved')
