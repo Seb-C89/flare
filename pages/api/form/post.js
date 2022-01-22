10a06c0fc5675f12f413cc67d8eb09785d51e659
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-01-22 15:22:11 +0100

    wip

--- pages/api/form/post.js
+++ pages/api/form/post.js
@@ -1,10 +1,10 @@
 const os = require('os'); // remove
-import { fileTypeFromFile } from 'file-type'
+import { fileTypeFromBuffer } from 'file-type'
 const fs = require('fs')
 const busboy = require('busboy');
 const path = require('path');
 var concat = require('concat-stream')
-//import { insert_post } from '../../../utils/db.js'
+import { insert_post } from '../../../utils/db.js'
 
 export const config = {
 	api: {
@@ -23,6 +23,7 @@ export default async function endpoint(req, res) {
 
 	if(!req.hasOwnProperty('body'))
 		req.body = {}
+	req.body.files = []
 
 	const bb = busboy({ headers: req.headers });
 		// for each uploaded file
@@ -32,52 +33,52 @@ export default async function endpoint(req, res) {
 			// save only file uploaded in the good field
 			if(name === field_for_file ){
 				console.log(`File [${name}]: filename: %j, encoding: %j, mimeType: %j`, filename, encoding, mimeType);
-				let filepath = path.join(save_dir, Date.now().toString()+Math.trunc(Math.random()).toString())
+				let filename = Date.now().toString()+Math.trunc(Math.random()).toString()
+				let filepath = path.join(save_dir, filename)
 				
 				//save file on hard drive
 				//file.pipe(fs.createWriteStream(filepath))
 				
 				//concat file
 				var concatStream = concat({ encoding: 'buffer' }, (file)=>{
-					if(file.length)
+					if(file.length){
 						fs.writeFile(filepath, file, (err) => {
 							if (err) throw err;
 							console.log('The file has been saved!');
 						});
+						
+						// file attribut
+						let ext = null
+
+						// analyse file
+						await fileTypeFromBuffer(file)
+							.then((res) => {
+								ext = res.ext
+								//fs.rename(files.file.filepath, files.file.filepath+'_'+res.ext)
+							}).catch(() => {
+								console.log('Failled to detecte file type for'+filepath)
+							})
+						
+						// add file to the body
+						req.body.files.push({
+							name: filename,
+							ext: ext,
+							client_name: info.filename
+						})
+					}
 					else
 						console.log("FILE IS EMPTY")
 				})
 				file.pipe(concatStream)
 				
 				// listen file event
-				file.on('data', (data) => {
+				/*file.on('data', (data) => {
 					console.log(`File [${name}] got ${data.length} bytes`);
 				}).on('end', async () => {
-					/*// file attribut
-					let ext = null
-
-					// analyse file
-					await fileTypeFromFile(filepath)
-						.then((res) => {
-							ext = res.ext
-							//fs.rename(files.file.filepath, files.file.filepath+'_'+res.ext)
-						}).catch(() => {
-							console.log('Failled to detecte file type for'+filepath)
-						})
-					
-					// add file to the list
-					if(!Array.isArray(req.body[name]))
-						req.body[name] = []
-					req.body[name].push({
-						path: filepath,
-						ext: ext,
-						client_name: info.filename
-					})*/
 
 				}).on('close', () => {
 					console.log(`File [${name}] done`);
-					/*console.log(file.read())*/
-				});
+				});*/
 			}
 			else{
 				console.log(`file ${name} discarded`)
@@ -91,13 +92,24 @@ export default async function endpoint(req, res) {
 			req.body[name] = val
 		});
 			
-		bb.on('close', () => {
+		bb.on('close', async () => {
 			console.log('Done parsing form!');
-			//res.writeHead(303, { Connection: 'close', Location: '/' });
-			//res.end();
+			// MYSQL
+			await insert_post(req.body.game, req.body.files)
+				.then(()=>{
+					res?.status(202).end()
+				})
+				.catch(()=>{
+					res?.status(503).end()
+				})
 		});
 
-    req.pipe(bb);
+    try {
+		req.pipe(bb);
+	} catch (e) {
+		console.log("BUSBOY EXECPTION")
+		res?.status(500).end()
+	}
 
 	console.log(req.body)
 	//await insert_post(post, file)
