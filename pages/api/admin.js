ddd7d0af74178425d5859eee43062ed9488e4c75
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-01 14:24:17 +0100

    api admin

--- pages/api/admin.js
+++ pages/api/admin.js
@@ -1,40 +1,25 @@
 //import { BASIC, buildWWWAuthenticateHeader, mechanisms } from 'http-auth-utils' // or https://www.npmjs.com/package/basic-auth
-import { setCookies, getCookie } from "cookies-next";
-import { generate_token, add_token, get_profil, print_all_tokens } from "../../utils/auth";
+import { setCookies, getCookies } from "cookies-next";
+import { generate_token, get_profil, is_admin, print_all_tokens } from "../../utils/auth";
 
 export default async function endpoint(req, res) {
-	let cookie = getCookie('token', { req, res})
-	//if(!cookie){
-		console.log(cookie)
-		if(!(get_profil(cookie) === "admin"))
-		{
-			let expiration = new Date()
-				expiration.setDate(expiration.getDate() +1)
-			let token = {
-				expire: expiration,
-				token: generate_token(),
-				profil: "admin"
-			}
+	crypto.pbkdf2("admin", '::salt::', 100000, 32, 'sha256', function(err, hashedPassword) {
+		console.log(hashedPassword)
+	})
 
-			add_token(token)
-			setCookies("token", token.token, {req, res, expires: expiration})
+	let {token, user, password} = getCookies({ req, res})
 
-			print_all_tokens()
-		} else
-			console.log("already admin")
-	//}
+	if(!(get_profil(token) === "admin"))
+		if(is_admin(user, password))
+			setCookies("token", generate_token("admin"), {req, res, expires: expiration})
+		else
+			console.log("bad credential")
+	else
+		console.log("already admin")
 
+	//print_all_tokens()
 	res.status(202).end()
 	return 
 }
 
 // https://medium.com/@greg.farrow1/nextjs-https-for-a-local-dev-server-98bb441eabd7
-
-//var token = crypto.randomBytes(64).toString('hex');
-
-/*crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-	if (err) { return cb(err); }
-	if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-	  return cb(null, false, { message: 'Incorrect username or password.' });
-	}
-	return cb(null, row);*/
--- utils/auth.js
+++ utils/auth.js
@@ -2,18 +2,40 @@ const crypto = require('crypto')
 
 let tokens = []
 
-export function generate_token() {
-	return crypto.randomBytes(12).toString('hex');
+export function generate_token(profil) {
+	let token = crypto.randomBytes(12).toString('hex');
+	let expiration = new Date()
+		expiration.setDate(expiration.getDate() +1)
+
+	tokens[token] = {
+		token: token,
+		expire: expiration,
+		profil: profil
+		// TODO may be add adresse ip to detecte stolen token
+	}
+
+	return token
 }
 
-export function add_token(token) {
+/*export function add_token(token) {
 	tokens[token.token] = token
 	console.log("ADD NEW TOKEN", token)
+}*/
+
+export function is_admin(user, password){
+	let check1 = false, check2 = false
+	
+	check1 = crypto.timingSafeEqual(process.env.ADMIN_USER, user)
+
+	crypto.pbkdf2(password, '::salt::', 100000, 32, 'sha256', function(err, hashedPassword) {
+		if (err) { return false }
+		check2 = crypto.timingSafeEqual(process.env.ADMIN_PASSWORD, hashedPassword)
+	})
+	
+	return check1 && check2;
 }
 
 export function get_profil(token) {
-	console.log("check profil")
-	console.log(tokens.hasOwnProperty(token), tokens[token])
 	if (tokens.hasOwnProperty(token))
 		return tokens[token].profil
 	else
