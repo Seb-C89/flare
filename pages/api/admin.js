4efc9ac475397965fe03dfab98dba6b3d978ffbc
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-04 21:15:09 +0100

    wip

--- pages/api/admin.js
+++ pages/api/admin.js
@@ -1,32 +1,12 @@
-//import { BASIC, buildWWWAuthenticateHeader, mechanisms } from 'http-auth-utils' // or https://www.npmjs.com/package/basic-auth
-import { setCookies, getCookie } from "cookies-next";
-import { generate_token, get_profil, is_admin, print_all_tokens } from "../../utils/auth";
+import { is_admin } from "../../utils/auth";
+import { withSessionRoute } from "../../utils/withIronSession.js"
 
-export default async function(req, res) {
-	/*crypto.pbkdf2("admin", '::salt::', 100000, 32, 'sha256', function(err, hashedPassword) {
-		console.log(hashedPassword, hashedPassword.length)
-	})*/
-
-	let token = getCookie('token', { req, res})
-
-	if(!(get_profil(token) === "admin"))
-		if(is_admin(req.body?.user, req.body?.password)){
-			let t = generate_token("admin")
-			setCookies("token",t.token , {req, res, expires: t.expires})
-			res?.status(202).end()
-			return {profil: t.profil} 
-		}
-		else {
-			console.log("bad credential")
-			res?.status(403).end()
-			return false
-		}
-	else {
-		console.log("already admin")
-		res?.status(202).end()
-		return {profil: get_profil(token)} 
-	}
-	//print_all_tokens()
-}
-
-// https://medium.com/@greg.farrow1/nextjs-https-for-a-local-dev-server-98bb441eabd7
+export default withSessionRoute((req, res) => {
+	// get user from database then:
+	req.session.user = {
+		id: 230,
+		admin: true,
+	};
+	await req.session.save();
+	res.send("Logged in");
+});
