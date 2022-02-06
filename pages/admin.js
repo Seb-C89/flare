e68163268b5e750963d0566216e2cc7ebbb8aacd
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-06 20:35:39 +0100

    early error handeling

--- pages/admin.js
+++ pages/admin.js
@@ -10,22 +10,24 @@ export default function(props){
 
 export const getServerSideProps = withSessionSsr(async (context) => {
 	let files_without_post, posts_without_files
+	let error = ""
 
 	if(context.req?.session?.admin){
 		await get_files_without_post().then((data) => {
 			files_without_post = data
-		}).catch(() => console.log("erorororororro"))
+		}).catch((x) => error.concat(x))
 
 		await get_posts_without_files().then((data) => {
 			data.forEach((element) => element.date = element.date.getTime()) // nextjs do not serialize date object.
 			posts_without_files = data  
-		}).catch(() => console.log("erorororororro"))
+		}).catch((x) => error.concat(x))
 	}
 
 	return {
 			props: {
 				files_without_post: files_without_post ?? null,
 				posts_without_files: posts_without_files ?? null,
+				error: error ?? null
 			}
 		}
 })
