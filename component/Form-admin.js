1871d6629e89f51dace02059b113872fea44dd63
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-12 13:04:12 +0100

    wip

--- component/Form-admin.js
+++ component/Form-admin.js
@@ -34,37 +34,71 @@ export default function(props) {
 	return <form ref={ form_ref } id="Form" method="DELETE" action="/admin" onSubmit={onSubmit}>
 		<section>
 			<p>{props.error}</p>
-			<p>File without related post</p>
-			<ul>
-				{ props.files_without_post?.map((x) => {
-					return <li><input type="checkbox" id={x.id} name="files_without_post" value={x.name} />
-							<label htmlFor="files_without_post">{x.name}</label></li>
-				}) }
-			</ul>
-			<p>Post without related file</p>
-			<ul>
-				{ props.posts_without_files?.map((x) => {
-					return <li><input type="checkbox" id={x.id} name="posts_without_files" value={x.game} />
-							<label htmlFor="posts_without_files">{x.game}</label></li>
-				}) }
-			</ul>
-			<p>Files uploaded but not regitered in Database</p>
-			<ul>
-			{ props.files_not_registered?.map((x) => {
-					return <li><input type="checkbox" id={x} name="files_not_registered" value={x} />
-							<label htmlFor="files_not_registered">{x}</label></li>
-				}) }
-			</ul>
-			<p>Files in Database but not on Disk</p>
-			<ul>
-			{ props.files_lost?.map((x) => {
-					return <li><input type="checkbox" id={x.id} name="files_lost" value={x.name} />
-							<label htmlFor="files_lost">{x.name}</label></li>
-				}) }
-			</ul>
+			<fieldset>
+				<legend>Post à valider</legend>
+				<table>
+				<thead>
+					<tr>
+						<th>Post</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ props.files_without_post?.map((x) => {
+						return <tr>
+								<td><label htmlFor="files_without_post">{x.name}</label></td>
+								<td><select name="files_without_post" id="pet-select">
+									<option value="">--Action--</option>
+									<option value="VAL">Valider</option>
+									<option value="DEL">Supprimer</option>
+								</select></td>
+							</tr>
+					}) }
+				</tbody>
+				</table>
+			</fieldset>
+			<fieldset>
+				<legend>File without related post</legend>
+				<p>Sélectionner les fichiers à supprimer</p>
+				<ul>
+					{ props.files_without_post?.map((x) => {
+						return <li><input type="checkbox" id={x.id} name="files_without_post" value={x.name} />
+								<label htmlFor="files_without_post">{x.name}</label></li>
+					}) }
+				</ul>
+			</fieldset>
+			<fieldset>
+				<legend>Post without related file</legend>
+				<p>Sélectionner les fichiers à supprimer</p>
+				<ul>
+					{ props.posts_without_files?.map((x) => {
+						return <li><input type="checkbox" id={x.id} name="posts_without_files" value={x.game} />
+								<label htmlFor="posts_without_files">{x.game}</label></li>
+					}) }
+				</ul>
+			</fieldset>
+			<fieldset>
+				<legend>Files on Disk <em>(publique or uploads directory)</em> but not regitered in Database</legend>
+				<p>Sélectionner les fichiers à supprimer</p>
+				<ul>
+				{ props.files_not_registered?.map((x) => {
+						return <li><input type="checkbox" id={x} name="files_not_registered" value={x} />
+								<label htmlFor="files_not_registered">{x}</label></li>
+					}) }
+				</ul>
+			</fieldset>
+			<fieldset>
+				<legend>Files in Database but not on Disk <em>(publique or uploads directory)</em></legend>
+				<p>Sélectionner les fichiers à supprimer</p>
+				<ul>
+				{ props.files_lost?.map((x) => {
+						return <li><input type="checkbox" id={x.id} name="files_lost" value={x.name} />
+								<label htmlFor="files_lost">{x.name}</label></li>
+					}) }
+				</ul>
+			</fieldset>
 			<div>
-				<p>Valider les posts séléctionnés et supprimer les fichiers séléctionnés:</p>
-				<input id="submit" type="submit" value="Valider et Supprimer" disabled={ submited }/>
+				<input id="submit" type="submit" value="Sauvegarder les changements" disabled={ submited }/>
 			</div>
 			{ result() }
 		</section>
