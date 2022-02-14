0af88e69d15bd3011e484fcde6e1607d638ade51
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-15 00:07:24 +0100

    ok

--- pages/api/image/[name].js
+++ pages/api/image/[name].js
@@ -1,35 +1,28 @@
 import { withSessionRoute } from "../../../utils/withIronSession"
 const fs = require('fs')
-import { get_file_by_name } from "../../../utils/db"
 
 export default withSessionRoute(async (req, res) => {
 	let { name } = req.query
 
-	{} await get_file_by_name(name)
-		.then(data => {
-			return data
-		})
-		.catch(err => {
-			
-		})
+	let files_in_uploads = fs.readdirSync("uploads")
 
-
-	try {
-		const file = fs.openSync("./uploads/"+name)
-		const stat = fs.fstatSync(file)
-		const stream = fs.createReadStream(null, {fd: file})
-			stream.on('finish', () => {
-				res.end()
-			})
-		res.setHeader('Content-Length', stat.size);
-		res.setHeader('Content-Type', 'image/jpg'); // TODO MIME
-		stream.pipe(res)
-	} catch (e){
-		console.log(e)
+	if(files_in_uploads.includes(name)){ // ensure that the file requested is in the desired directory and avoid requesting other file
+		try {
+			const file = fs.openSync("./uploads/"+name)
+			const stat = fs.fstatSync(file)
+			const stream = fs.createReadStream(null, {fd: file})
+				stream.on('finish', () => {
+					res.end()
+				})
+			res.setHeader('Content-Length', stat.size);
+			res.setHeader('Content-Type', 'image/jpg'); // TODO MIME
+			stream.pipe(res)
+		} catch(e){
+			console.log(e)
+			res.status(404).end()
+		}
+	} else
 		res.status(404).end()
-	}
 	
 	//res.setHeader('Content-Disposition', 'attachment; filename=your_file_name');
-	//res.write(file, 'binary');
-	//res.end()
 });
