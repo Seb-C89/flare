2afcfb93d6cdf47cc873337b31f6fe9ebcce7f34
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-24 22:40:50 +0100

    ok

--- pages/api/file.js
+++ pages/api/file.js
@@ -1,31 +1,51 @@
 import { withSessionRoute } from "../../utils/withIronSession"
-import { rm } from "fs"
+//import { rmSync } from "fs"
+import { sql_query } from "../../utils/db"
 
 export default withSessionRoute(async (req, res) => {
 	if(req.session.admin){
-		console.log("RECEIVED")
-		console.log(req.body)
-
 		/* ensure each property is an array and parse JSON for each element */
 		for(const p in req.body){
-			if(!Array.isArray(req.body[p])){
-				console.log("become array")
+			if(!Array.isArray(req.body[p]))
 				req.body[p] = [req.body[p]]
-			}
 
-			req.body[p] = req.body[p].map(e => {
-				console.log("aaaaaaaaaa", e)
-				console.log(JSON.parse(e))
-				return JSON.parse(e)
-			})
+			req.body[p] = req.body[p].map(e => JSON.parse(e))
 		}
-
-		console.log(req.body)
 		
 		let { files_without_post, posts_without_files, files_lost, files_not_registered } = req.body
 
-		for(const f of files_not_registered){
-			try{
+		/* posts_without_files */
+		for(const x of posts_without_files){
+			sql_query("SELECT * FROM post WHERE id=? AND game=?", [x.id, x.game])
+			//sql_query("DELETE FROM file WHERE id=? AND name=?", [x.id, x.name])
+				.then(data => console.log("POST DELETED", data))
+				.catch(e => {console.log(e); return res.status(404).end()})
+		}
+		
+		/* files_without_post */
+		for(const x of files_without_post){
+			sql_query("SELECT * FROM file WHERE id=? AND name=?", [x.id, x.name])
+			//sql_query("DELETE FROM file WHERE id=? AND name=?", [x.id, x.name])
+				.then(data => {
+					console.log("FILES WITHOUT POST DELETED", data)
+					// TODO delete file, but now the directory of the file is unknown (public or upload ?)
+					//console.log(`${path} removed`)
+					//rmSync(f.path)
+				})
+				.catch(e => {console.log(e); return res.status(404).end()})
+		}
+
+		/* files_lost */
+		for(const x of files_lost){
+			sql_query("SELECT * FROM file WHERE id=? AND name=?", [x.id, x.name])
+			//sql_query("DELETE FROM file WHERE id=? AND name=?", [x.id, x.name])
+				.then(data => console.log("FILE LOST DELETED", data))
+				.catch(e => {console.log(e); return res.status(404).end()})
+		}
+
+		/* files_not_registered */
+		try{
+			for(const f of files_not_registered){
 				switch(f.directory){
 					case "upload":
 						f.path = process.env.DIR_UPLOAD_IMG+f.name
@@ -37,15 +57,13 @@ export default withSessionRoute(async (req, res) => {
 						throw `Wrong directory: ${f.directory}`
 				}
 				console.log(`${f.path} removed`)
-			}catch(e){
-				console.log(e)
+				//rmSync(f.path)
 			}
+		}catch(e){
+			console.log(e)
+			return res.status(404).end()
 		}
 
-		/*[Object: null prototype] {
-		files_without_post: [ 'fdbhftnhjdf', 'fwsdbhw' ],
-		files_lost: [ 'fdbhftnhjdf', 'fwsdbhw', 'hvgcn' ]
-		}*/
 		res.status(202).end()
 	} else
 		res.status(404).end()
