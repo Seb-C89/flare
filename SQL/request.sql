326b23dfbe4c977df9f48684f01453f23f4bfb9b
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-28 23:59:58 +0100

    wip

--- SQL/request.sql
+++ SQL/request.sql
@@ -11,29 +11,7 @@ select * from file left join post on post.id = file.post where post.id is null;
 /* post without file */
 select * from post where post.id not in (select file.post from file)
 
-<fieldset>
-				<legend>Post à valider</legend>
-				<table>
-				<thead>
-					<tr>
-						<th>Post</th>
-						<th>Valider</th>
-					</tr>
-				</thead>
-				<tbody>
-					{ props.files_without_post?.map((x) => {
-						return <tr>
-								<td><label htmlFor={x.name}>{x.name}</label></td>
-								<td><select name={x.name} id={x.id}>
-									<option value="">--Action--</option>
-									<option value="VAL">Valider</option>
-									<option value="DEL">Supprimer</option>
-								</select></td>
-							</tr>
-					}) }
-				</tbody>
-				</table>
-			</fieldset>
+UPDATE post SET status="OK" WHERE id=23;
 
-			var file = fs.readFile(filePath, 'binary');
+
 			
