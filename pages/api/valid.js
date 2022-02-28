ec68ddbcf0b61da2e0da95c63205b6096c2d97a4
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-02-28 23:51:52 +0100

    wip

--- pages/api/valid.js
+++ pages/api/valid.js
@@ -2,8 +2,26 @@ import { withSessionRoute } from "../../utils/withIronSession"
 
 export default withSessionRoute((req, res) => {
 	if(req.session.admin){
-		console.log("kjhgyfds", req.body)
+		console.log(req.body)
+		try{
+			for(const p in req.body){
+				let post = JSON.parse(p)
+				switch(req.body[p]){
+					case 'VAL':
+						console.log("VAL", post)
+						break;
+					case 'DEL':
+						console.log("DEL", post)
+						break;
+					default:
+						console.log("NOTHING")
+						break;
+				}
+			}
+		} catch(e) {
+			console.log(e)
+		}
 		res.status(202).end()
-	}
-	res.status(404).end()
+	} else
+		res.status(404).end()
 })
