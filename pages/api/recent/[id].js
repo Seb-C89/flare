8b181b6df2133798b7129edcf880083c27f9e5c3
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-03-02 17:46:13 +0100

    ok

--- pages/api/recent/[id].js
+++ pages/api/recent/[id].js
@@ -1,10 +1,18 @@
-import { sql_query } from "../../../utils/db"
+import { sql_query, get_file_from_post } from "../../../utils/db"
 
 export default async function(req, res) {
 	let { id } = req.query
 
-	let data = id 	? await sql_query("SELECT * FROM posts WHERE id > ? LIMIT 10", id)
-					: await sql_query("SELECT * FROM posts LIMIT 10")
+	let data = id 	? await sql_query("SELECT * FROM post WHERE id > ? LIMIT 10", id)
+					: await sql_query("SELECT * FROM post WHERE LIMIT 10")
 	
-	res.status(200).json(data)
+	let post = await Promise.all(data.map(async x => {
+		let file = await get_file_from_post(x.id)
+		return {
+			posts: x,
+			file: file
+		}
+	}))
+
+	res.status(200).json(post)
 }
