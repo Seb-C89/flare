032331c8716838696a435ecb3ddbef2b53cf971b
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-03-03 18:06:13 +0100

    better

--- component/Gallery.js
+++ component/Gallery.js
@@ -1,9 +1,10 @@
 import Card from "./Card.js"
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function({ posts, post_per_page }) {
 
 	const [get_more_button_ref] = React.useState(React.createRef());
+	const [get_more_link_ref] = React.useState(React.createRef());
 	const [cards, setCards] = React.useState({	cards: posts.map(x => <Card post={x} />),
 												last_id: posts[posts.length-1]?.post.id,
 												added_cards: posts.length });
@@ -33,10 +34,19 @@ export default function({ posts, post_per_page }) {
 			console.log("NETWORK ERROR", e)
 		})
 	}
+
+	/*	effect to hide/reveal the good button
+		link to the next page if no javascript,
+		button to get more post if javascript */
+	useEffect(()=>{
+		get_more_button_ref.current.style.display = 'initial'
+		get_more_link_ref.current.style.display = 'none'
+	}, [get_more_link_ref]) // block the hook, so it will be executed only once
+
 	console.log(cards.added_cards, post_per_page)
 	return <section id="Gallery">
 		{ cards.cards }
-		{ cards.added_cards == post_per_page ? <a href={"/index/"+cards.last_id}>Next</a> : <></> }
-		{ cards.added_cards == post_per_page ? <button onClick={get_more} ref={ get_more_button_ref }>Get More</button> : <></> }
+		{ cards.added_cards == post_per_page ? <a href={"/index/"+cards.last_id} ref={ get_more_link_ref } >Next</a> : <></> }
+		{ cards.added_cards == post_per_page ? <button onClick={get_more} ref={ get_more_button_ref } style={{display: 'none'}}>Get More</button> : <></> }
 	</section>
 }
