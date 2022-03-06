80d7a088b7596bfeaed956c89c897f6782e9c011
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2022-03-06 13:45:17 +0100

    good way

--- target/test3/script.js
+++ target/test3/script.js
@@ -1,9 +1,16 @@
+let flare1 = document.querySelector("#flare1")
+	flare1.style.display = 'unset'
+
 let middle_x = window.innerWidth / 2
 let middle_y = window.innerHeight / 2
 
-window.addEventListener('mousemove', e => {
+window.addEventListener('click', e => {
+	console.log(e.clientX, e.clientY)
 	let a = ((e.clientY - middle_y) / (e.clientX - middle_x))
 	let b = middle_y - a * middle_x
-	console.log("a", a)
-	console.log("b", b)
+	let solution = -b/a
+	console.log("width", window.innerWidth)
+	console.log("a", a, "b", b)
+	console.log("solution", -b/a)
+	flare1.style.left = `${solution}px`
 })
