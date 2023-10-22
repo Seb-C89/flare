3009ae170fa6e2eb10ce10e5731abfa961873a31
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2023-10-22 14:28:04 +0200

    [FIX] mysql connection closed for innactivity.

--- utils/db.js
+++ utils/db.js
@@ -2,22 +2,23 @@ import { renameSync } from 'fs'
 // TODO close connection
 
 var mysql      = require('mysql2');
-var connection = mysql.createConnection({
-host     : process.env.MYSQL_HOST,
-user     : process.env.MYSQL_USER,
-password : process.env.MYSQL_PASSWORD,
-database : process.env.MYSQL_DB,
-port     : process.env.MYSQL_PORT
+var connection = mysql.createPool({
+connectionLimit	: 1,
+host			: process.env.MYSQL_HOST,
+user			: process.env.MYSQL_USER,
+password		: process.env.MYSQL_PASSWORD,
+database		: process.env.MYSQL_DB,
+port			: process.env.MYSQL_PORT
 });
 
-connection.connect(
+/*connection.connect(
 	function(err) {
 		if (err)
 			console.error('error connecting: ' + err.stack);
 		else 
 			console.log('NEW CONNEXION connected as id ' + connection.threadId);
 	}
-);
+);*/
 
 export function sql_query(sql, arg){
 	return new Promise((resolve, reject) => {
