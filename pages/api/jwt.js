c5d82e0bcfe38ad8a417fa2323a425deb7b695bb
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2024-05-01 23:32:38 +0200

    wip

--- pages/api/jwt.js
+++ pages/api/jwt.js
@@ -9,11 +9,15 @@ export default async function test (req, res) {
 	//.setIssuedAt()
 	.setIssuer('seb@outllook.lol')
 	//.setAudience('urn:example:audience')
-	.setExpirationTime('1s')
-	.setNotBefore('3s')
+	.setExpirationTime('1m')
+	//.setNotBefore('3s')
 	.encrypt(secret)
 
 	console.log(jwt)
+	console.log("cook", req.cookies)
+	console.log("cook", res.cookies)
+
+	res.setHeader("Set-Cookie", `${process.env.COOKIES_NAME}=${jwt}`)
 
 	const a = await jose.jwtDecrypt(jwt, secret).then(res => { console.log("OK", res); return res }).catch(err => console.log("KO", err))
 	console.log("RESULT", a)
@@ -21,3 +25,18 @@ export default async function test (req, res) {
 	res.status(200)
 	res.send('<a href="/api/jwt2">next</a>')
 }
+
+function setCookie(res, cookieValue) {
+	if ("headers" in res && typeof res.headers.append === "function") {
+	  res.headers.append("set-cookie", cookieValue);
+	  return;
+	}
+	let existingSetCookie = res.getHeader("set-cookie") ?? [];
+	if (!Array.isArray(existingSetCookie)) {
+	  existingSetCookie = [existingSetCookie.toString()];
+	}
+	res.setHeader("set-cookie", [
+	  ...existingSetCookie,
+	  cookieValue
+	]);
+  }
