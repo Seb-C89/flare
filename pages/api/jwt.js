578bd47dbdcce42de2512972877818a7b80ecfc6
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2024-05-07 23:40:19 +0200

    for development only

--- pages/api/jwt.js
+++ pages/api/jwt.js
@@ -2,21 +2,24 @@ import * as jwe from "../../utils/jwt"
 
 /* to know if an user is connected. see _app.js ~168 */
 export default async function test (req, res) {
-	const session = await jwe.getSession(req)
-	if(req.query.start){
-		let new_session = jwe.createUserSession(req.query.start)
-		await jwe.saveSession(res, new_session)
-	}
-	if(req.query.gets){
-		console.log("GETS", session)
-	}
-	if(req.query.close){
-		await jwe.closeSession(res, session)
-	}
-	if(req.query.mod){
-		session.mod = req.query.mod
-		console.log("MOD", session)
-		await jwe.saveSession(res, session)
-	}
-	res.json(session)
+	if(process.env.NODE_ENV === "development") {
+		const session = await jwe.getSession(req)
+		if(req.query.start){
+			let new_session = jwe.createUserSession(req.query.start)
+			await jwe.saveSession(res, new_session)
+		}
+		if(req.query.gets){
+			console.log("GETS", session)
+		}
+		if(req.query.close){
+			await jwe.closeSession(res, session)
+		}
+		if(req.query.mod){
+			session.mod = req.query.mod
+			console.log("MOD", session)
+			await jwe.saveSession(res, session)
+		}
+		res.json(session)
+	} else
+		res.status(404).end()
 }
