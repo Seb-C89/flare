96e15af981765b3dcf15e7556a7388bcb9e7f9c6
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2024-05-12 13:41:20 +0200

    fix redirection

--- pages/auth.js
+++ pages/auth.js
@@ -1,16 +1,15 @@
 import { useRouter } from 'next/navigation'
-
 import { withSessionSsr, unseal_mail_perm} from "../utils/withIronSession"
 
 /* Landing page from connection, change the react state to connected, create session over cookie, and redirect */
 // TODO change adresse in mail
 
-export default function auth(props) {
-	//console.log(props)
+export default function Auth(props) {
+	const router = useRouter()
 	props.setAuth(props.logged);
 	if(props.redirect)
 		if (typeof window !== 'undefined') { // TODO Why it work ? (remove if statement)
-			useRouter().push(props.redirect, undefined, {shallow: false})
+			router.push('/'+props.redirect)
 		}
 	return <></>
 }
